90516e2c324a8b2bcd31715477fc0becd9971515
Jenkins.Lorenzo@gmail.com <Lorenzo@DESKTOP-IMT9BSL>
2018-05-23 05:08:14 -0400

    added style sheet and installed bootstrap

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -16,9 +17,23 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // Todo: Email response to the party orginzer
+                return View("Thanks", guestResponse);
+            } else {
+                // there is a validation error
+                return View();
+            }
         }
     }
 }
